ee3bb17317fcc771986c97dbbc25f58fc0297bb5
SylEmo <sylvain.emonet@orange.fr>
2015-11-27 00:48:50 +0100

    Ajout du Temps dans le Syso de départ. A réutiliser partout ?

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,4 @@
+import java.text.SimpleDateFormat;
 import java.util.Date;
 
 /**
@@ -17,16 +18,21 @@ public class Main {
 	 * @param args
 	 */
 	public static void main(String[] args) {
+		
+		Date tempsLancement = new Date();
+		SimpleDateFormat formater = null;
+		formater = new SimpleDateFormat("dd/MM/yyyy 'à' hh:mm:ss:SSS");
+		System.out.println(formater.format(tempsLancement));
+		
 		/***** Algo 3 *****/
 		KMPAlgorithm kmp = new KMPAlgorithm(pattern, sequence);
 		kmp.doYourJob();
 		
 		/***** Algo 4 *****/
-		SuffixTree st = new SuffixTree(sequenceSuffixTree);
-		st.showMeThings();		
+		//SuffixTree st = new SuffixTree(sequenceSuffixTree);
+		//st.showMeThings();		
 		
 		BWTAlgorithm bwt = new BWTAlgorithm(pattern);
 		bwt.makeMeDream();
 	}
-
 }
