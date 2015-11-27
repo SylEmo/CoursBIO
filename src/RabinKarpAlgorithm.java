9231b704e24f17ec9724e831b5deff33adce267a
baptistetrulla <baptiste.trulla@gmail.com>
2015-11-27 09:53:23 +0100

    Algo Rabin Karp avec chrono

--- src/RabinKarpAlgorithm.java
+++ src/RabinKarpAlgorithm.java
@@ -2,33 +2,115 @@
  * @author Sylvain EMONET & Baptiste TRULLA
  *
  */
-public class RabinKarpAlgorithm {
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+import java.util.Date;
+import java.util.Random;
+import java.math.BigInteger;
+import java.text.SimpleDateFormat;
 
-	
-	public final static int q = 11;
-	
-	public RabinKarpAlgorithm(String T, String M){
-		
-		int n = T.length();
-		int m = M.length();
-		
-		int hn = hash(T.substring(1,m));
-		int hm = hash(M.substring(1, m));
-		
-		for (int i = 0; i < n-m+1; i++) {
-			if (hn == hm)
-			{
-				if ( T.substring(i, i+m-1) == M.substring(1,m)){
-					System.out.println("résultat trouvé en position : "+i);
-				}
+public class RabinKarpAlgorithm {
+	private String pattern;
+	private long patternHash;
+	/** pattern length **/
+	private int M;
+	private long Q;
+	private long RM;
+
+	static final String text = "GAATTCGAGATGCGAATGAGCAGCAGCCATTTTGATGTTGTGAGCATCGGAACGTTTCTGCG\n"
+			+ "TCCGTACACTGTCCTTTTGTTACTTAGATAATGGCTAAGGCAAGCAGTCCGGGCCACAGGAGTCAAAG"
+			+ "GCTTTTCGCCAGCTCCTAAACGCTGGAAGTGTAATTTTTTTTCTTCTTATAAAATTAAACAAACCCTT\n"
+			+ "TTAGAAAGGAACACTCGCTTTATCTCTTCGACCGAATTTACTATACATGGATATATATATATTATCTTC"
+			+ "TGTTCACAGTTAAAACTAGGAATAGCATAGTCATAAGTTAACACCATCATGTTGAGAACGTCAACATTG\n"
+			+ "TTCACCAAGCGTGTCCAACCAAGCCTATTTTCTAGAAACATTCTTAGATTGCAATCCACAGCTGCAATC"
+			+ "CCTAAGACTCAAAAAGGTGTCATCTTTTATGAGAATAAGGGGAACCTGCATTACAAAGATATCCCTGTC\n"
+			+ "CCCGAGCCTAAGCCAAATGAAATTTTAATCAACGTTAAATATTCTGGTGTATGTCACACCGATTTACAT"
+			+ "GCTTGGCACGGCGATTGGCCATTACCTGTTAAACTACCATTAGTAGGTGGTCATGAAGGTGCTGGTGTA\n"
+			+ "GTTGTCAAACTAGGTTCCAATGTCAAGGGCTGGAAAGTCGGTGATTTAGCAGGTATCAAATGGCTGAAC"
+			+ "GGTTCTTGTATGACATGCGAATTCTGTGAATCAGGTCATGAATCAAATTGTCCAGATGCTGATTTATCT\n"
+			+ "GGTTACACTCATGATGGTTCTTTCCAACAATTTGCGACCGCTGATGCTATTCAAGCCGCCAAAATTCAA"
+			+ "CAGGGTACCGACTTGGCCGAAGTAGCCCCAATATTATGTGCTGGTGTTACTGTATATAAAGCACTAAAA\n"
+			+ "GAGGCAGACTTGAAAGCTGGTGACTGGGTTGCCATCTCTGGTGCTGCAGGTGGCTTGGGTTCCTTGGCC"
+			+ "GTTCAATATGCAACTGCGATGGGTTACAGAGTTCTAGGTATTGATGCAGGTGAGGAAAAGGAAAAACTTT\n"
+			+ "TCAAGAAATTGGGGGGTGAAGTATTCATCGACTTTACTAAAACAAAGAATATGGTTTCTGACATTCAAGA"
+			+ "AGCTACCAAAGGTGGCCCTCATGGTGTCATTAACGTTTCCGTTTCTGAAGCCGCTATTTCTCTATCTACG\n"
+			+ "GAATATGTTAGACCATGTGGTACCGTCGTTTTGGTTGGTTTGCCCGCTAACGCCTACGTTAAATCAGAGG"
+			+ "TATTCTCTCATGTGGTGAAGTCCATCAATATCAAGGGTTCTTATGTTGGTAACAGAGCTGATACGAGAGA\n"
+			+ "AGCCTTAGACTTCTTTAGCAGAGGTTTGATCAAATCACCAATCAAAATTGTTGGATTATCTGAATTACCA"
+			+ "AAGGTTTATGACTTGATGGAAAAGGGCAAGATTTTGGGTAGATACGTCGTCGATACTAGTAAATAATAGC\n"
+			+ "GTGTTACGCACCCAAACTTTTATGAAAGTCTTTGTTTATAATGATGAGGTTTATAAATATATAGTGGAGC"
+			+ "AAAGATTAATCACTAAATCAAGAAGCAGTACCAGTATTTTTTCTATATCAAGTAGTGATAATGGAAATAG\n" + "CCCAAATTTGGCTTCCGTCGAC";
+
+	public RabinKarpAlgorithm(String text, String pattern) {
+		this.pattern = pattern;
+		M = pattern.length();
+		Q = 11;
+		RM = 1;
+
+		for (int i = 1; i <= M - 1; i++)
+			RM = (RM) % Q;
+
+		patternHash = hash(pattern, M);
+		search(text);
+	}
+
+	public long hash(String key, int M) {
+		long i = 0;
+		for (int j = 0; j < M; j++)
+			i = (i + key.charAt(j)) % Q;
+		return i;
+	}
+
+	public boolean check(String txt, int i) {
+		for (int j = 0; j < M; j++)
+			if (txt.charAt(i + j) !=pattern.charAt(j))
+				return false;
+		return true;
+	}
+
+	public void search(String text) {
+		int N = text.length();
+		if (N > M) {
+			long textHash = hash(text, M);
+
+			/** hash match, then exact match **/
+			for (int i = M; i < N; i++) {
+				textHash = (textHash + Q - RM * text.charAt(i - M) % Q) % Q;
+				textHash = (textHash + text.charAt(i)) % Q;
+				// match
+				int offset = i - M + 1;
+				if ((patternHash == textHash) && check(text, offset))
+					System.out.println(pattern + " trouvé à la position : " + i + ".");
+
 			}
-			hn = hash(T.substring(i+1, i+m));
 		}
 	}
-	
-	public int hash (String T){
-		int res = Integer.parseInt(T);
-		return res % q;
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		System.out.println("Rabin Karp Algorithme\n");
+		System.out.println("\nText utilisé : \n\n" + text);
+		
+		/** If we want to enter a sequence, uncomment the following line**/
+		// String text = br.readLine();
+		System.out.println("\nEntrer un Pattern\n");
+		String pattern = br.readLine();
+		
+		Date tempsAvantRKA = new Date();
+		SimpleDateFormat formater = null;
+		formater = new SimpleDateFormat("'le' dd/MM/yyyy 'à' hh:mm:ss:SS");
+		System.out.println("Début de l'algorithme Rabin Karp "+formater.format(tempsAvantRKA));
+		
+		System.out.println("\nResultats : \n");
+		RabinKarpAlgorithm rk = new RabinKarpAlgorithm(text, pattern);
+
+		Date tempsApresRKA = new Date();
+		System.out.println("\nFin de l'algorithme Rabin Karp "+formater.format(tempsApresRKA));
+
+		long chrono = tempsApresRKA.getTime() - tempsAvantRKA.getTime();
+		System.out.println("Temps total de l'éxécution : " + chrono + " ms");
+
 	}
-	
 }
